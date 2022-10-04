811affa5b581f12fb700eb51132b8ec719a1edb1
filipesaretta <filipessm124@gmail.com>
2022-10-04 18:25:15 -0300

    Initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  cyan300: "hsl(174, 77%, 80%)",
+  cyan500: "hsl(174, 86%, 45%)",
+  grayRed100: "hsl(14, 92%, 95%)",
+  red300: "hsl(15, 100%, 70%)",
+  blue500: "hsl(226, 100%, 87%)",
+
+  white: "hsl(0, 0%, 100%)",
+  paleBlue100: "hsl(230, 100%, 99%)",
+
+  grayBlue100: "hsl(224, 65%, 95%)",
+  grayBlue200: "hsl(223, 50%, 87%)",
+  grayBlue500: "hsl(225, 20%, 60%)",
+  grayBlue800: " hsl(227, 35%, 25%)",
+};
