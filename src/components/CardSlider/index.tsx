a99d84b88a1e8e0ec9e2c19ef3d578ee771ec750
filipesaretta <filipessm124@gmail.com>
2022-10-06 18:20:53 -0300

    Responsive and create slider

--- src/components/CardSlider/index.tsx
+++ src/components/CardSlider/index.tsx
@@ -7,25 +7,49 @@ import {
   Price,
   PaymentToggle,
   Toggle,
+  Details,
 } from "./style";
+
+import check from "../../assets/icon-check.svg";
 
 export function CardSlider() {
   return (
     <Card>
       <SliderSection>
         <PriceSection>
-          <PageViews>100k PAGEVIEWS</PageViews>
-          <Slider>Slider</Slider>
+          <PageViews>100K PAGEVIEWS</PageViews>
+          <Slider>
+            <span>
+              <input type="range" />
+            </span>
+          </Slider>
           <Price>
             <span>$16.00</span>/month
           </Price>
         </PriceSection>
         <PaymentToggle>
           <span>Monthly Billing</span>
-          <Toggle></Toggle>
+          <Toggle>
+            <input type="checkbox" />
+            <span></span>
+          </Toggle>
           <span>Yearly Billing 25% discount</span>
         </PaymentToggle>
       </SliderSection>
+      <Details>
+        <div>
+          <span>
+            <img src={check} alt="" /> Unlimited websites
+          </span>
+          <span>
+            <img src={check} alt="" /> 100% data ownership
+          </span>
+          <span>
+            <img src={check} alt="" /> Email reports
+          </span>
+        </div>
+        <button>Start my trial</button>
+      </Details>
     </Card>
   );
 }
--- src/components/CardSlider/style.ts
+++ src/components/CardSlider/style.ts
@@ -1,10 +1,18 @@
 import styled from "styled-components";
+import iconSlider from "../../assets/icon-slider.svg";
 
 export const Card = styled.div`
-  background-color: ${(props) => props.theme.white};
+  max-width: 500px;
+  position: relative;
+  padding: 1.5rem;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.05), 0 4px 4px rgba(0, 0, 0, 0.1);
-  padding: 3rem;
+  background-color: ${(props) => props.theme.white};
   border-radius: 8px;
+  margin: 0 1rem;
+
+  @media (min-width: 525px) {
+    width: 500px;
+  }
 `;
 
 export const PriceSection = styled.div`
@@ -20,34 +28,158 @@ export const PriceSection = styled.div`
   }
 `;
 
-export const SliderSection = styled.div``;
+export const SliderSection = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 2rem;
+`;
 
 export const PageViews = styled.p`
-  grid-column: 1;
+  grid-column: 1 / -1;
   grid-row: 1;
+  justify-self: center;
+  text-transform: uppercase;
+
+  @media (min-width: 525px) {
+    justify-self: start;
+    grid-column: 1;
+  }
 `;
 
 export const Slider = styled.div`
   grid-column: 1 / -1;
   grid-row: 2;
+  height: 3rem;
+  display: grid;
+  align-items: center;
+
+  input {
+    -webkit-appearance: none;
+    appearance: none;
+    height: 0.875rem;
+    background-color: ${(props) => props.theme.cyan300};
+    border-radius: 8px;
+    width: 100%;
+
+    &::-webkit-slider-thumb {
+      background: url(${iconSlider}) no-repeat center;
+      position: relative;
+      -webkit-appearance: none;
+      appearance: none;
+      height: 2.2rem;
+      width: 2.2rem;
+      border-radius: 100%;
+      background-color: ${(props) => props.theme.cyan500};
+    }
+  }
 `;
 
 export const Price = styled.p`
-  grid-column: 2;
-  grid-row: 1;
-  justify-self: end;
+  grid-column: 1 / -1;
+  grid-row: 3;
+  justify-self: center;
+
+  @media (min-width: 525px) {
+    grid-row: 1;
+    justify-self: end;
+    grid-column: 2;
+  }
 `;
 
 export const PaymentToggle = styled.div`
-  display: flex;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  gap: 1rem;
+  justify-content: center;
+
   font-size: 0.875rem;
   color: ${(props) => props.theme.grayBlue800};
 `;
 
-export const Toggle = styled.div`
+export const Toggle = styled.label`
+  position: relative;
   width: 45px;
   height: 20px;
-  border-radius: 15px;
-  background-color: gainsboro;
+
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  input:checked + span:before {
+    transform: translateX(22px);
+  }
+
+  input:checked + span {
+    background-color: ${(props) => props.theme.cyan500};
+  }
+
+  span {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    transition: 0.4s;
+    border-radius: 15px;
+
+    &:before {
+      position: absolute;
+      content: "";
+      height: 15px;
+      width: 15px;
+      left: 4px;
+      bottom: 3px;
+      background-color: white;
+      transition: 300ms ease-in;
+      border-radius: 50%;
+      background-color: ${(props) => props.theme.white};
+    }
+  }
+`;
+
+export const Details = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 2rem;
+  margin-top: 3rem;
+  padding-bottom: 1rem;
+
+  @media (min-width: 525px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  button {
+    background-color: ${(props) => props.theme.grayBlue800};
+    border: none;
+    color: ${(props) => props.theme.blue500};
+    border-radius: 50px;
+    height: 50px;
+    width: 80%;
+    justify-self: center;
+    align-self: center;
+    cursor: pointer;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+
+    @media (min-width: 525px) {
+      align-items: flex-start;
+      gap: 0.5rem;
+    }
+
+    span {
+      display: grid;
+      grid-template-columns: max-content 1fr;
+      align-items: center;
+      gap: 0.5rem;
+    }
+  }
 `;
